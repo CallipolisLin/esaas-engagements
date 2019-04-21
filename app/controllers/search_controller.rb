ced4bf423acd6ba7099f1348917e5a09203d2d7b
Sarah Lin <rhapsodylin@berkeley.edu>
2019-04-20 18:25:47 -0700

    iter2 feature5 finished

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,20 +3,36 @@ class SearchController < ApplicationController
   def search
     keyword = params["keyword"]
     filters = params[:search_filters]
-    redirect_to results_path(:keyword => keyword, :filters => filters)
+    @all_filters = ["App", "Organization", "User"]
+    if keyword.empty?
+      flash[:notice] = "Please enter a keyword in the search box."
+      redirect_to results_path(:keyword => "", :search_filters => @all_filters) and return 
+    elsif filters.nil?
+      flash[:notice] = "Please at least choose one category you want to search."
+      redirect_to results_path(:keyword => "", :search_filters => @all_filters) and return 
+    end
+    flash[:notice] = "Search #{keyword} according to the filters #{params[:search_filters].map{|x| x.inspect}.join(', ')}."
+    redirect_to results_path(:keyword => keyword, :search_filters => filters)
   end
 
   def results
-    keyword = "%" + params["keyword"] + "%"
+    keyword = ("%" + params["keyword"] + "%").downcase
     filters = params[:search_filters]
-    if !filters['App'].nil?
-      @apps = App.where('name LIKE ?', keyword).all()
+    if filters.include?("App")
+      @apps = App.where('lower(name) LIKE ?', keyword).all() + App.where('lower(description) LIKE ?', keyword).all()
+    else
+      @apps = []
     end
-    if (filters['Document'] == 1)
-      @orgs = Org.where('name LIKE ?', keyword).all()
+    if filters.include?("Organization")
+      @orgs = Org.where('lower(name) LIKE ?', keyword).all()
+    else
+      @orgs = []
     end
-    if (filters['User'] == 1)
-      @users = User.where('name LIKE ?', keyword).all()
+    if filters.include?("User")
+      @users = User.where('lower(name) LIKE ?', keyword).all()
+    else
+      @users = []
     end
   end
 end
+
